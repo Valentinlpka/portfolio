86c8564fb378ad7002e1b0bf2a67b8604fceed95
Valentinlpka <lipkavalentin@outlook.fr>
2023-01-07 16:58:55 +0100

    new lines in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,16 +32,16 @@ const navLinks = [
         location: document.getElementById('aboutSection').getBoundingClientRect().top,
     },
     {
-        link: document.getElementById('homeLink'),
-        location: 0,
+        link: document.getElementById('skillLink'),
+        location: document.getElementById('skill').getBoundingClientRect().top,
     },
     {
-        link: document.getElementById('homeLink'),
-        location: 0,
+        link: document.getElementById('projetsLink'),
+        location: document.getElementById('projets').getBoundingClientRect().top,
     },
     {
-        link: document.getElementById('homeLink'),
-        location: 0,
+        link: document.getElementById('contactLink'),
+        location: document.getElementById('contact').getBoundingClientRect().top,
     },
 ];
 
@@ -51,15 +51,30 @@ let activeLink = false;
 const checkActiveLinks = () => {
     const scroll = window.scrollY + window.innerHeight / 2;
 
-    console.log(scroll);
     for (let i = 0; i < navLinks.length; i++) {
         if (navLinks[i].location < scroll) {
             activeLink = navLinks[i].link;
+            activeLink.classList.add('active');
+        } else {
+            activeLink.classList.remove('active');
         }
-
-    }
+        } 
 
 }
 
 checkActiveLinks();
+
 window.addEventListener('scroll', checkActiveLinks);
+
+
+var app = document.getElementById('textBonjour');
+
+var typewriter = new Typewriter(app, {
+  loop: false,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(900)
+  .typeString("LIPKA VALENTIN")
+  .start();
