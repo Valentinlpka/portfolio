2f110008621a75bec68e9c568284956f5282c7e9
Valentinlpka <lipkavalentin@outlook.fr>
2023-01-08 23:31:56 +0100

    new style for a to do list

--- todolist/script.js
+++ todolist/script.js
@@ -1,58 +1,59 @@
-const input = document.getElementById('texte');
-const validate = document.getElementById('valider');
-const todolist = document.getElementById('todolist');
+const input = document.getElementById("texte");
+const validate = document.getElementById("valider");
+const todolist = document.getElementById("todolist");
 
-validate.addEventListener('click', () => {
-
-    // Create an "li" node:
-const div = document.createElement("div");
-div.classList.add("list");
-// Create a text node:
-
-const p = document.createElement("p");
-p.textContent = input.value;
-
-
-
-const button2 = document.createElement("button");
-button2.classList.add("delete");
-button2.textContent = 'X';
-
-const button1 = document.createElement("button");
-button1.classList.add("validate");
-button1.textContent = 'V';
-
-
-// Append the text node to the "li" node:
-div.appendChild(p);
-div.appendChild(button1);
-div.appendChild(button2);
-
-// Append the "li" node to the list:
-document.getElementById("todolist").appendChild(div);
-
-input.value ='' 
-
-let supp = document.getElementsByClassName("delete")
-
-var i;
-for (i = 0; i < supp.length; i++) {
-  supp[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+validate.addEventListener("click", (event) => {
+  let localItems = JSON.parse(localStorage.getItem("localItem"));
+  if (localItems === null) {
+    taskList = [];
+  } else {
+    taskList = localItems;
   }
+  taskList.push(input.value);
+  localStorage.setItem("localItem", JSON.stringify(taskList));
+  showlist();
+  event.preventDefault();
+
+  input.value = "";
+});
+
+function showlist() {
+  let outPut = "";
+  let taskListShow = document.getElementById("todolist");
+  let localItems = JSON.parse(localStorage.getItem("localItem"));
+  if (localItems === null) {
+    taskList = [];
+  } else {
+    taskList = localItems;
+  }
+  taskList.forEach((data, index) => {
+    outPut += `<div class="list">
+    <p>${data}</p>
+    <button class="delete" onClick="deleteItem(${index})">X</button>
+    </div>`;
+  });
+  taskListShow.innerHTML = outPut;
+}
+showlist()
+
+function deleteItem(index) {
+  let localItems = JSON.parse(localStorage.getItem("localItem"));
+  taskList.splice(index, 1);
+  localStorage.setItem("localItem", JSON.stringify(taskList));
+  showlist();
+
 }
 
-let valider = document.getElementsByClassName("validate")
 
+function updateItem() {
+let valider = document.getElementsByClassName("validate");
 var i;
 for (i = 0; i < valider.length; i++) {
-  valider[i].onclick = function() {
+  valider[i].onclick = function () {
     var div = this.parentElement;
+    console.log(this.parentElement);
     div.style.color = "white";
-    div.classList.toggle("souligne")
-  }
+    div.classList.toggle("souligne");
+  };
 }
-
-})
-
+}
